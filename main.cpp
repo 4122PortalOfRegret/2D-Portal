6a15dff701276982983fc92beaa1b5e8246903ee
kmcclelland3 <kristy97@comcast.net>
2018-05-02 16:36:58 -0700

    level 9 finalized

--- main.cpp
+++ main.cpp
@@ -858,33 +858,33 @@ void level9(SDL_Renderer* renderer, vector<Block>& blockVec, EndZoneWall& end, P
     SDL_Rect grayBot2 = {290,680,140,30};
     SDL_Rect grayBot3 = {570,680,140,30};
     SDL_Rect grayBot4 = {850,680,140,30};
-    SDL_Rect grayBot5 = {1130,680,140,30};
+    //SDL_Rect grayBot5 = {1130,680,140,30};
 
-    SDL_Rect topWall1 = {140,10,140,350};
-    SDL_Rect botWall1 = {140,360,140,350};
+    SDL_Rect topWall1 = {150,10,140,350};
+    SDL_Rect botWall1 = {150,460,140,250};
     SDL_Rect topWall2 = {430,10,140,200};
     SDL_Rect botWall2 = {430,310,140,400};
     SDL_Rect topWall3 = {710,10,140,400};
-    SDL_Rect botWall3 = {710,450,140,260};
-    SDL_Rect botWall4 = {1130,150,140,560};
+    SDL_Rect botWall3 = {710,510,140,200};
+    SDL_Rect botWall4 = {990,150,140,560};
 
     Block g1(10,10,140,30,renderer, grayTop1, false);
     Block g2(290,10,140,30,renderer, grayTop2, false);
-    Block g3(570,10,140,30,renderer, grayTop2, false);
+    Block g3(570,10,140,30,renderer, grayTop3, false);
     Block g4(850,10,420,30,renderer, grayTop4, false);
     Block g5(10,680,140,30,renderer, grayBot1, false);
     Block g6(290,680,140,30,renderer, grayBot2, false);
     Block g7(570,680,140,30,renderer, grayBot3, false);
     Block g8(850,680,140,30,renderer, grayBot4, false);
-    Block g9(1130,680,140,30,renderer, grayBot5, false);
+    //Block g9(1130,680,140,30,renderer, grayBot5, false);
 
-    Block b1(140,10,140,350,renderer, topWall1, true);
-    Block b2(140,360,140,350,renderer, botWall1, true);
+    Block b1(150,10,140,350,renderer, topWall1, true);
+    Block b2(150,460,140,250,renderer, botWall1, true);
     Block b3(430,10,140,200,renderer, topWall2, true);
     Block b4(430,310,140,400,renderer, botWall2, true);
     Block b5(710,10,140,400,renderer, topWall3, true);
-    Block b6(710,450,140,260,renderer, botWall3, true);
-    Block b7(1130,150,140,560,renderer, botWall4, true);
+    Block b6(710,510,140,200,renderer, botWall3, true);
+    Block b7(990,150,140,560,renderer, botWall4, true);
 
     blockVec.push_back(leftwall);
     blockVec.push_back(rightwall);
@@ -898,7 +898,7 @@ void level9(SDL_Renderer* renderer, vector<Block>& blockVec, EndZoneWall& end, P
     blockVec.push_back(g6);
     blockVec.push_back(g7);
     blockVec.push_back(g8);
-    blockVec.push_back(g9);
+    //blockVec.push_back(g9);
     blockVec.push_back(b1);
     blockVec.push_back(b2);
     blockVec.push_back(b3);
@@ -912,7 +912,7 @@ void level9(SDL_Renderer* renderer, vector<Block>& blockVec, EndZoneWall& end, P
     }
 
     end.setX(1170);
-    end.setY(670);
+    end.setY(646);
     p.setX(75);
     p.setY(640);
 
